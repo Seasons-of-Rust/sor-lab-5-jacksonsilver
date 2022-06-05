a4eea3482593e67f6dbcd1ca5949ed11b5390445
jacksonsilver <jacksonsilver@cmail.carleton.ca>
2022-06-04 20:42:56 -0400

    Update main.rs

--- space-agency/src/main.rs
+++ space-agency/src/main.rs
@@ -1,5 +1,6 @@
 use personnel::AstronautJob;
 use personnel::Candidate;
+use std::cmp::Reverse;
 
 fn get_job_code(job: &AstronautJob) -> i32 {
     match job {
@@ -14,23 +15,26 @@ fn get_job_code(job: &AstronautJob) -> i32 {
     }
 }
 
-fn calculate_job_score(_candidate: &Candidate) -> i32{
+fn calculate_job_score(_candidate: &Candidate) -> i32 {
     let job_score = get_job_code(&_candidate.primary_job)
         * match &_candidate.secondary_job {
             None => get_job_code(&_candidate.primary_job),
-            Some(v) => get_job_code(&v),
+            Some(v) => get_job_code(v),
         };
     job_score % 576
 }
 
-fn calculate_candidate_score(_candidate: &Candidate) -> i32{
-    let candidate_score: i32 = (_candidate.health as i32 + calculate_job_score(&_candidate)) * _candidate.age as i32;
+fn calculate_candidate_score(_candidate: &Candidate) -> i32 {
+    let candidate_score: i32 =
+        (_candidate.health as i32 + calculate_job_score(_candidate)) * _candidate.age as i32;
     candidate_score % 3928
 }
 
 fn main() {
     let mut _candidates: Vec<Candidate> = Candidate::load_candidate_file();
 
-    _candidates.sort_by(|a, b| calculate_candidate_score(b).cmp(&calculate_candidate_score(a)));
+    // _candidates.sort_by(|a, b| calculate_candidate_score(b).cmp(&calculate_candidate_score(a)));
+    // Cargo Clippy told me to do this:
+    _candidates.sort_by_key(|b| Reverse(calculate_candidate_score(b)));
 
 }
