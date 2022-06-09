2b6409b10531b96ab1edfb285f7ed12b4f1b792f
jacksonsilver <jacksonsilver@cmail.carleton.ca>
2022-06-08 20:49:14 -0400

    Update lib.rs

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,10 +1,15 @@
+
+
 #[cfg(test)]
+
+use personnel::Candidate;
+use personnel::AstronautJob;
 mod tests {
     #[test]
     fn it_works() {
         let a = Candidate {
             primary_job: AstronautJob::Biogeochemist, 
-            secondary_job: None, 
+            secondary_job: AstronautJob::Biogeochemist, 
             age: 25, 
             health: 72};
         assert_eq!(calculate_job_score(&a), 217);
